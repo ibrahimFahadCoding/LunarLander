c39171504c8998fdf46b682f24400a1043bb95ae
Ibrahim Fahad <ibrahimthecoder123@gmail.com>
2024-01-02 20:22:49 -0800

    Updated Code so Model loads trained agent

--- main.py
+++ main.py
@@ -4,31 +4,24 @@ from stable_baselines3.common.vec_env import DummyVecEnv
 
 # Create the Lunar Lander environment
 env = DummyVecEnv([lambda: gym.make('LunarLander-v2', render_mode="human")])
-print("env made")
+print("[*] Env Made")
 
-# Define and train the PPO model
-model = PPO("MlpPolicy", env, verbose=1)
-print("Model created")
-model.learn(total_timesteps=100000)
-print("Model trained")
+model = PPO.load("ppo_lunar_lander")
 
-
-
-# Save the trained model
-model.save("ppo_lunar_lander")
+print("[*] Model Loaded")
 
 # Evaluate the trained agent
 total_rewards = 0
 num_eval_episodes = 10
 
 for _ in range(num_eval_episodes):
-    obs = env.reset()
+    state = env.reset()
     done = False
 
     while not done:
         env.render()
-        action, _ = model.predict(obs)
-        obs, reward, done, _ = env.step(action)
+        action, _ = model.predict(state)
+        state, reward, done, _ = env.step(action)
         print(f'Reward: {reward}')
         total_rewards += reward
 
